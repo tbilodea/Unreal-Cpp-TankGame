a9f4d6db81cd664430db84870cfb2d9b9eec9def
tbilodeau <tbilodea@gmail.com>
2020-04-16 22:15:31 -0600

    Added Force on the tracks

---
+++ TankGame/Source/TankGame/Private/TankTracks.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankTracks.h"
+
+void UTankTracks::SetThrottle(float Throttle)
+{
+    float ClampedThrottle = FMath::Clamp(Throttle, -1.f, +1.f);
+
+    auto ForceApplied = GetForwardVector() * ClampedThrottle * MaxDrivingForce;
+    auto ForceLocation = GetComponentLocation();
+    auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+    TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
+    UE_LOG(LogTemp, Warning, TEXT("Tank is throttled"));
+}
+
---
+++ TankGame/Source/TankGame/Public/TankTracks.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTracks.generated.h"
+
+/**
+ * Use to apply movement forces
+ */
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TANKGAME_API UTankTracks : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Speed);
+
+private:
+	//Max force in N
+	UPROPERTY(EditDefaultsOnly)
+	float MaxDrivingForce = 400000.f;
+};
